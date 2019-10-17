6b926ca9b5349a17b10de57e37236697f45dd756
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-17 14:03:30 +0200

    script works

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // ma c’è uno sconto del 20% per i minorenni
 // e del 40% per gli over 65
 
-var km,eta,prezzo,under18,over65,prezzoscontato18,prezzoscontato65;
+var km,eta,prezzo,under18,over65,prezzoScontato18,prezzoScontato65;
 // chiedo numero km
 km = prompt("Quanti chilometri devi percorrere?");
 
@@ -14,34 +14,23 @@ km = prompt("Quanti chilometri devi percorrere?");
 eta = prompt("Quanti anni hai?");
 
 // prezzo
-prezzo = 0.21* km;
-console.log(prezzo);
+prezzo = 2* km;
+console.log("prezzo standard " + prezzo);
 
 // sconto under18
 under18 = prezzo / 100 * 20 ;
-console.log(under18);
-//over65
+console.log("sconto per under18 : " + under18);
+// sconto over65
 over65 = prezzo / 100 * 40 ;
-console.log(over65);
+console.log("sconto per over65 : " + over65);
 
 if(eta < 18){
-  prezzoscontato18 = prezzo - under18;
-  console.log(prezzoscontato18);
+  prezzoScontato18 = prezzo - under18;
+  console.log("questo è il prezzo per i minorenni: " + prezzoScontato18);
 }else if(eta > 65){
-  prezzoscontato65 = prezzo - over65;
-  console.log(prezzoscontato65);
+  prezzoScontato65 = prezzo - over65;
+  console.log("questo è il prezzo per gli over65: " + prezzoScontato65);
+}else{
+  prezzo = prezzo;
+  console.log(prezzo);
 }
-//
-// prezzoover65 = prezzo - 40%
-//
-// if(eta < 18){
-//   prezzo = prezzominorenni ;
-//   console.log(prezzo)
-// }if else(eta > 65){
-//   prezzo = prezzoover65;
-//   console.log(prezzo)
-// }else{
-//   prezzo = prezzo;
-//   console.log(prezzo)
-//
-// }
