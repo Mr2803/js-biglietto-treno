34033ef3dcf3264bb6601588dbbb8705c013ff9b
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-17 16:39:01 +0200

    try to apply discount "silente"

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 
 
 // dichiarazione variabili
-var km,eta,prezzoStandard,under18,over65,prezzoScontato18,prezzoScontato65;
+var km,eta,prezzoStandard,under18,over65,silente,prezzoScontato18,prezzoScontato65,prezzoScontatoSilente;
 
 // chiedo numero km
 km = parseInt(prompt("Quanti chilometri devi percorrere?"));
@@ -30,19 +30,25 @@ console.log("sconto per under18 : " , under18 , "€");
 over65 = parseFloat(prezzoStandard / 100 * 40) ;
 console.log("sconto per over65 : " , over65 , "€");
 
+// sconto silente
+silente = parseFloat(prezzoStandard / 100 * 100);
+
 if(eta < 18){
   prezzoScontato18 = prezzoStandard - under18;
   console.log("questo è il prezzo per gli under18: " , prezzoScontato18 , "€");
+  document.getElementById('ticket').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoScontato18 + "€" ;
 
 }else if(eta > 65){
   prezzoScontato65 = prezzoStandard - over65;
   console.log("questo è il prezzo per gli over65: " , prezzoScontato65 , "€");
+  document.getElementById('ticket').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoScontato65 + "€" ;
 
+}else if(eta>65 && eta>100){
+  prezzoScontatoSilente = prezzoStandard - silente;
+  // console.log("questo è il prezzo per gli over65: " , prezzoScontato65 , "€");
+  document.getElementById('ticket').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoScontatoSilente + "€" ;
 }else{
   prezzoStandard = prezzoStandard;
   console.log("tu paghi : " , prezzoStandard , "€");
+  document.getElementById('ticket').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoStandard + "€";
 }
-
-document.getElementById('ticket').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoStandard ;
-document.getElementById('ticket18').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoScontato18 ;
-document.getElementById('ticket65').innerHTML = "Salve babbano il prezzo del tuo biglietto è: " + prezzoScontato65 ;
