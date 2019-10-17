570f52aaf5fee494d662c1759e041fa3825846fb
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-17 13:20:53 +0200

    create basic structure

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// ll programma dovrà chiedere all’utente il numero di chilometri
+// e l’età
+// e dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 €cent al km)
+// ma c’è uno sconto del 20% per i minorenni
+// e del 40% per gli over 65
+
+// chiedo numero km
+var km = prompt("Quanti chilometri devi percorrere?")
+
+
+// chiedo età
+var eta = prompt("Quanti anni hai?")
+
+// prezzo
+var prezzo = 0.21* km
+console.log(prezzo)
